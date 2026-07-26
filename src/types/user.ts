6e6a85083bb26343bf342e3aa56ad786092107e1
Oghomena <oghomenag@gmail.com>
2026-07-26 09:15:00 +0100

    initialize project

---
+++ src/types/user.ts
@@ -0,0 +1,46 @@
+export type UserStatus = "active" | "inactive" | "pending" | "blacklisted";
+
+export interface Guarantor {
+  fullName: string;
+  phoneNumber: string;
+  email: string;
+  relationship: string;
+}
+
+export interface User {
+  id: string;
+  organization: string;
+  username: string;
+  email: string;
+  phoneNumber: string;
+  dateJoined: string;
+  status: UserStatus;
+  tier: 1 | 2 | 3;
+  accountBalance: number;
+  accountNumber: string;
+  bankName: string;
+  profile: {
+   firstName: string;
+   lastName: string;
+   gender: string;
+   bvn: number;
+   maritalStatus: string;
+   children: number;
+   typeOfResidence: string
+  };
+  education: {
+    level: string;
+    employmentStatus: string;
+    sector: string;
+    durationOfEmployment: string;
+    officeEmail: string;
+    monthlyIncome: [number, number];
+    loanRepayment: number;
+  };
+  socials: {
+    twitter: string;
+    facebook: string;
+    instagram: string;
+  };
+  guarantors: Guarantor[];
+}
